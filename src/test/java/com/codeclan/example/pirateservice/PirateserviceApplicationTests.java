6c0fc67fbba31efa99b3f508fecbe6079ab67530
beckyjn <rebeccajoynielsen@gmail.come>
2019-05-20 18:00:50 +0100

    homework done

---
+++ src/test/java/com/codeclan/example/pirateservice/PirateserviceApplicationTests.java
@@ -0,0 +1,46 @@
+package com.codeclan.example.pirateservice;
+
+import com.codeclan.example.pirateservice.models.Pirate;
+import com.codeclan.example.pirateservice.models.Raid;
+import com.codeclan.example.pirateservice.models.Ship;
+import com.codeclan.example.pirateservice.repositories.PirateRepository;
+import com.codeclan.example.pirateservice.repositories.RaidRepository;
+import com.codeclan.example.pirateservice.repositories.ShipRepository;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class PirateserviceApplicationTests {
+
+	@Autowired
+	PirateRepository pirateRepository;
+
+	@Autowired
+	ShipRepository shipRepository;
+
+	@Autowired
+	RaidRepository raidRepository;
+
+	@Test
+	public void contextLoads() {
+	}
+
+	@Test
+	public void createPirateAndShipAndRaid() {
+		Ship ship = new Ship("The Flying Dutchman");
+		shipRepository.save(ship);
+
+		Pirate pirate1 = new Pirate("Jack", "Sparrow", 32, ship);
+		Raid raid1 = new Raid("Tortuga", 2000);
+		pirate1.addRaid(raid1);
+
+		pirateRepository.save(pirate1);
+		raidRepository.save(raid1);
+
+	}
+
+}
